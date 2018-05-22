967c1f95e8ae2794de51ffe63930744244963efe
Vanessa <cup0ft3a12@gmail.com>
2018-05-22 16:11:25 +0800

    create home page

---
+++ js/script.js
@@ -0,0 +1,40 @@
+
+
+$(document).ready(function () {
+    var trigger = $('.hamburger'),
+        overlay = $('.overlay'),
+        isClosed = false;
+
+
+    trigger.click(function () {
+        hamburger_cross();
+
+    });
+
+   
+    function hamburger_cross() {
+        if (isClosed == true) {  
+            overlay.hide();
+            trigger.removeClass('is-open');
+            trigger.addClass('is-closed');
+            isClosed = false;
+        } else {   
+            overlay.show();
+            trigger.removeClass('is-closed');
+            trigger.addClass('is-open');
+            isClosed = true;
+        }
+    }
+    
+    $('[data-toggle="offcanvas"]').click(function () {
+        $('#wrapper').toggleClass('toggled');
+    }); 
+    
+    $('.overlay').on('click', function (){
+        $('#wrapper').removeClass('toggled');
+        overlay.hide();
+             hamburger_cross();
+    });
+
+}); 
+
